a445d12d7b7c7a858d696f6728b84015921d2594
tumijacob <matome.phoshoko@jembi.org>
2017-09-15 15:34:54 +0200

    Use angularJS without external libraries This is an alternative solution to the first one in that it uses only angularJS without any jquery images and library

--- js/loadingIndicator.js
+++ js/loadingIndicator.js
@@ -1,30 +1,13 @@
 'use strict'
 
 var app = angular.module('loadingIndicatorApp', [])
-.directive('loading', ['$http', function ($http) {
-  return {
-    restrict: 'A',
-    template: '<div class="loading-spiner"><img src="http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif" /> </div>',
-    link: function (scope, elm, attrs) {
-      scope.isLoading = function () {
-        return $http.pendingRequests.length > 0
-      }
-
-      scope.$watch(scope.isLoading, function (v) {
-        if (v) {
-          elm.show()
-        } else {
-          elm.hide()
-        }
-      })
-    }
-  }
-}])
 app.controller('loadingIndicatorCtrl', function ($scope, $http) {
   $scope.loadData = function () {
+    $scope.loading = true
     $http.jsonp('http://www.filltext.com/?callback=JSON_CALLBACK&rows=5&delay=5&fname={firstName}&lname={lastName}')
       .success(function (data) {
         $scope.developers = data
+        $scope.loading = false
       })
   }
 })
